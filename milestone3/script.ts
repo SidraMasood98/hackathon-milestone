73578cfb484e43b5d886e682a1ee4a253450a4f2
Sidra Masood <160876510+SidraMasood98@users.noreply.github.com>
2024-12-30 10:22:27 +0500

    Add files via upload

---
+++ milestone3/script.ts
@@ -0,0 +1,44 @@
+//  get reference to the form and display area
+
+const form = document.getElementById("Resume-form") as HTMLFormElement
+const resumeDisplayElement = document.getElementById('resume-display') as HTMLDivElement
+
+// Handle form Submission
+form.addEventListener('submit' , (event : Event) => {
+    event.preventDefault(); // prevent page reload
+
+
+// collect input values;
+const names = (document.getElementById('name') as HTMLInputElement).value
+const email = (document.getElementById('email') as HTMLInputElement).value
+const phone = (document.getElementById('phone') as HTMLInputElement).value
+const education = (document.getElementById('education') as HTMLInputElement).value
+const experience = (document.getElementById('experience') as HTMLInputElement).value
+const skills = (document.getElementById('skills') as HTMLInputElement).value
+
+//  Generate the resume content dynamically
+
+const resumeHTML= `
+<h2><b>Resume</b></h2>
+<h3>Personal Information</h3>
+<p><b>Name:</b>${names}</p>
+<p><b>Email:</b>${email}</p>
+<p><b>Phone:</b>${phone}</p>
+
+<h3>Education</h3>
+<p>${education}</p>
+
+<h3>Experience</h3>
+<p>${experience}</p>
+
+<h3>Skills</h3>
+<p>${skills}</p>`
+
+// Display the Generated resume
+
+if (resumeDisplayElement){
+    resumeDisplayElement.innerHTML= resumeHTML;
+}else {
+    console.error('The resume display element is missing')
+}
+})
